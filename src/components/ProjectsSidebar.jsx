603646af16617ece9fae5b0ca605cdbcf21ccfa1
Luong Minh Nhat <luongminhnhat1604@gmail.com>
2025-08-11 18:23:09 +0700

    Added the sidebar structural skeleton

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,13 @@
+export default function ProjectsSidebar() {
+    return <aside>
+        <h2>Your projects</h2>
+        <div>
+            <button>
+                Add project
+            </button>
+        </div>
+        <ul>
+
+        </ul>
+    </aside>
+}
