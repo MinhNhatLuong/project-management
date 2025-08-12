c973180e5d325e5286c3e20aab64b50e5715aba9
Luong Minh Nhat <luongminhnhat1604@gmail.com>
2025-08-12 09:59:43 +0700

    Added handleClickAddProject funtion at App.jsx to deal with switch section logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleClickAddProject() {
+    setProjectsState(prevProjectsState => ({
+      ...projectsState,
+      selectedProjectId: null
+    }))
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onClickAddProject={handleClickAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <ProjectsSidebar onClickAddProject={handleClickAddProject} />
+      {content}
     </main>
   );
 }
